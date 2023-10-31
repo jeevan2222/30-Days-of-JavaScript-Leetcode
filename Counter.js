6027c6bf447c8ea93fcd81f6a6767cea2b3a7419
Jeevan501 <jeevana591501@gmail.com>
2023-10-31 10:31:33 +0530

    Counter

---
+++ Counter.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function (n) {
+  let num = n;
+  return function () {
+    return (num = num + 1);
+  };
+};
+
+const counter = createCounter(10);
+counter(); // 10
+// counter(); // 11
+// counter(); // 12
+console.log(counter());
